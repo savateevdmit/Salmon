749102eba32e0212d1f5eaea94cd3792c368a71a
savateevdmit <savateev.dmit@yandex.ru>
2022-03-17 17:15:05 +0300

    Попытка загрузки Pyaudio

--- main/bot.py
+++ main/bot.py
@@ -1,3 +1,10 @@
+try:
+    import os
+    os.system('pip install SpeechRecognition')
+    os.system('python PyAudio/setup.py install')
+except Exception:
+    pass
+
 import discord
 from discord.ext import commands
 from config import settings
