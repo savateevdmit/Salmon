6bf81d82013d54cdde3f4d07bfdd57ec9c51b849
savateevdmit <savateev.dmit@yandex.ru>
2022-03-21 22:56:11 +0300

    Реализована команда !help и игра быки и коровы (!bc)

--- main/bot.py
+++ main/bot.py
@@ -1,18 +1,22 @@
-# try:
-#     import os
-#     os.system('pip install SpeechRecognition')
-#     os.system('python PyAudio-0.2.11/setup.py install')
-# except Exception:
-#     pass
+from random import sample
 
+import discord
 from discord.ext import commands
-
+from bulls_and_cows import bulls_and_cows
 from config import settings
-from voice import voicee
+
+try:
+    import os
+
+    os.system('pip install SpeechRecognition')
+except:
+    pass
 
 a = False
-
+cycles = dict(game=True)
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 bot = commands.Bot(command_prefix=settings['prefix'])
+bot.remove_command('help')
 
 
 #####################################################
@@ -21,6 +25,11 @@ bot = commands.Bot(command_prefix=settings['prefix'])
 # async def start(ctx):  # Создаём функцию и передаём аргумент ctx.
 #     voicee(ctx)
 ######################################################
+
+@bot.command()
+async def stop_game(ctx):
+    print('jn')
+    cycles["game"] = False
 
 
 @bot.command()
@@ -32,6 +41,78 @@ async def join(ctx):
 @bot.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+
+
+@bot.command()
+async def bc(ctx):
+    embed = discord.Embed(title='❗Правила❗', color=0x4fde02)
+
+    embed.add_field(name='🐄 Быки и коровы 🐂',
+                    value='🔰 Бот загадывает число, состоящие из четырёх разных цифр. Задача угадать загаданное число. '
+                          'Отправляйте боту четырёхзначные числа. Результат каждого хода оценивается числом **БЫКОВ** '
+                          'и **КОРОВ**. Если совпадает цифра и её позиция, то это **БЫК**. Если цифра совпадает, '
+                          'но её позиция нет, то это **КОРОВА**.',
+                    inline=False)
+    embed.add_field(name='\u200b',
+                    value='**♻Игра уже началась, отправляйте числа!♻**',
+                    inline=False)
+    embed.set_image(url='https://lh3.googleusercontent.com/6oHWaM1Z9NELHkkO7VKjwrzkAlG-rTyHGhWJcCnxWHhfJhubkinI_PfnkKS-7bC3t_k=h500')
+    await ctx.send(embed=embed)
+    number = sample(range(1, 10), 4)
+    print(number)
+
+    while True:
+        message = await bot.wait_for('message', check=bc)
+        if message.content == '!stop_game':
+            await ctx.send('stoped')
+            return
+        else:
+            count = 0
+            id = message.author.id
+            if message.author.discriminator != '7918':
+                if len(message.content) == 4:
+                    for i in message.content:
+                        if i in numbers:
+                            count += 1
+                    if count == 4:
+                        if len(bulls_and_cows(int(message.content), number)) > 25:
+                            embed = discord.Embed(title='🐄 Быки и коровы 🐂', color=0xf25a07)
+                            embed.add_field(name='\u200b',
+                                            value='😥' + f'<@!{id}>' + f'**, {bulls_and_cows(int(message.content), number)}**',
+                                            inline=False)
+                            embed.set_footer(text='Продолжайте!')
+                            await message.channel.send(embed=embed)
+                        else:
+                            embed = discord.Embed(title='🐄 Быки и коровы 🐂', color=0x4fde02)
+                            embed.add_field(name='\u200b',
+                                            value='🎉' + f'<@!{id}>' + f'**, {bulls_and_cows(int(message.content), number)}**',
+                                            inline=False)
+                            count += 1
+                            await message.channel.send(embed=embed)
+                            await ctx.send('stoped')
+                            return
+
+
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(color=0x08e7f7)
+
+    embed.set_author(name='📒 Справкa')
+
+    embed.add_field(name='`!play`', value='Проигрывание музыки', inline=True)
+    embed.add_field(name='`!stop`', value='Остановка музыки', inline=True)
+    embed.add_field(name='`!join`', value='Присоединение бота к голосовому каналу', inline=True)
+    embed.add_field(name='`!bc`', value='Запуск игры "Быки и коровы"', inline=True)
+    embed.add_field(name='`!meme`', value='Показ рандомного мема из интернета', inline=True)
+    embed.add_field(name='`!logo`', value='Угадывание названия компании по логотипу на время', inline=True)
+    embed.add_field(name='`!stop game`', value='Остановка игры', inline=True)
+
+    embed.add_field(name='\u200b',
+                    value='Более подробную информацию ищите здесь - https://clck.ru/eAsPG',
+                    inline=False)
+    # embed.add_field(name="\u200B", value='<https://github.com/savateevdmit/Salmon.git>', inline=False)
+    await ctx.send(embed=embed)
 
 
 bot.run(settings['token'])  # Обращаемся к словарю settings с ключом token, для получения токена
