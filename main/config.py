f30823e931a42289706d69c89b4832a85b2b4a9d
savateevdmit <savateev.dmit@yandex.ru>
2022-04-11 11:54:20 +0300

    Сделал кнопки для музыки, вывод чартов и очереди, воспроизведение чартов, добавил возможность скачивания музыки (только при наличии премиума)

--- main/config.py
+++ main/config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.ui7Nbwly66EzVcOxaUKOnR_DB9o',
+    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.gxVB-fGvfAwB3k-wo__jPmP09fQ',
     'bot': 'Salmon',
     'id': 953018464652652675,
     'prefix': '!',
--- main/voice.py
+++ main/voice.py
@@ -1,59 +1,26 @@
 import asyncio
-import os  # работа с файловой системой
-
-import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
-
-recognizer = speech_recognition.Recognizer()
-microphone = speech_recognition.Microphone()
+from config import settings
+from discord.ext import commands
+from discord.ext.audiorec import NativeVoiceClient
+bot = commands.Bot(command_prefix=settings['prefix'])
 
 
-def record_and_recognize_audio(*args: tuple):
-    with microphone:
-        recognized_data = ""
-        recognizer.adjust_for_ambient_noise(microphone, duration=2)
-
-        try:
-            audio = recognizer.listen(microphone, 6, 6)
-            with open("microphone-results.wav", "wb") as file:
-                file.write(audio.get_wav_data())
-        except speech_recognition.WaitTimeoutError:
-            return
-        try:
-            print("Started recognition...")
-            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
-        except speech_recognition.UnknownValueError:
-            pass
-        except speech_recognition.RequestError:
-            print("Trying to use offline recognition...")
-
-        return recognized_data
-
-
-def voicee(ctx):
+def voice(ctx):
     async def main(ctx):
-        print('jhb')
+        channel: discord.VoiceChannel = ctx.author.voice.channel  # type: ignore
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect(cls=NativeVoiceClient)
         while True:
-
-            # старт записи речи с последующим выводом распознанной речи
-            # и удалением записанного в микрофон аудио
-            try:
-                channel = ctx.author
-                voice_input = record_and_recognize_audio()
-                os.remove("microphone-results.wav")
-                if voice_input == 'стоп':
-                    break
-                elif 'иди' in voice_input and 'лосось' in voice_input:
-                    cha = ctx.author.voice.channel
-                    if cha != None:
-                        await cha.connect()
-                        continue
-                    continue
-                elif 'уйди' in voice_input and 'лосось' in voice_input and channel.voice != None:
-                    await ctx.voice_client.disconnect()
-                    continue
-                await ctx.send(voice_input)
-
-            except:
-                pass
+            ctx.voice_client.record(lambda e: print(f"Exception: {e}"))
+            await ctx.send(f'Start Recording')
+            await asyncio.sleep(10)
+            wav_bytes = await ctx.voice_client.stop_record()
+            await ctx.send(f'Stop Recording')
+            f = open("rec.txt")
+            lines = f.readlines()
+            if 'быки' or 'коровы' in lines[0]:
+                await ctx.invoke(bot.get_command('bc'))
 
     asyncio.get_running_loop().create_task(main(ctx))
