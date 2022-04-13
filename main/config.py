f72400c461846a0db45d4cd6a4aaf8dc6121c449
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2022-04-13 22:13:53 +0300

    Поменяли токен

--- main/config.py
+++ main/config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.gxVB-fGvfAwB3k-wo__jPmP09fQ',
+    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.ge45VSdmm2EjrKvgXRuSCl6fK0I',
     'bot': 'Salmon',
     'id': 953018464652652675,
     'prefix': '!',
