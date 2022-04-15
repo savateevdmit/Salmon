3b147c46bc3c5d21572eabdd224789ed3ad4bd34
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2022-04-15 15:51:04 +0300

    Сделал игру ним-пасьянс с 3 кучами. Поменяли токен

--- main/config.py
+++ main/config.py
@@ -1,7 +1,8 @@
 settings = {
-    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.ge45VSdmm2EjrKvgXRuSCl6fK0I',
+    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.8juN2t_gRynFmODrqSSp-Sr2a7M',
     'bot': 'Salmon',
     'id': 953018464652652675,
     'prefix': '!',
-    'token_ya': 'AQAAAABe7a80AAG8XvG0qgliTEWbtRju9fqcoFg'
+    'token_ya': 'AQAAAABe7a80AAG8XvG0qgliTEWbtRju9fqcoFg',
+    'DATABASE_URL': 'postgres://czbfshfsmvgtdh:e2f26fe5aef54f983261ed6c870ec70a3418805bcf59bf8bb3b1d667fc9cd30d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/df66fnp05ac88e'
 }
