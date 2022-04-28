f40875c7ef330fc69716183019216eb73a55fe82
SavD <73773381+savateevdmit@users.noreply.github.com>
2022-04-28 18:52:23 +0300

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,8 +1,8 @@
 settings = {
-    'token': 'OTUzMDE4NDY0NjUyNjUyNjc1.Yi-dnA.zcYesdBBW0dHmmDmNdZQtxSVf9U',
+    'token': 'тут должен быть ваш токен дискорд бота',
     'bot': 'Salmon',
     'id': 953018464652652675,
     'prefix': '!',
-    'token_ya': 'AQAAAABe7a80AAG8XvG0qgliTEWbtRju9fqcoFg',
-    'DATABASE_URL': 'postgres://czbfshfsmvgtdh:e2f26fe5aef54f983261ed6c870ec70a3418805bcf59bf8bb3b1d667fc9cd30d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/df66fnp05ac88e'
+    'token_ya': 'токен Яндекс Музыки',
+    'DATABASE_URL': 'URL базы данных'
 }
